77f61eb6101f6d4d9dc9f859ff38bd0f50d8b416
minhvd <minh.vu@fruitful.io>
2021-03-15 21:50:15 +1100

    add AbstractPageUI

---
+++ interfaces/pageUIs/AbstractPageUI.java
@@ -0,0 +1,21 @@
+package pageUIs;
+
+public class AbstractPageUI {
+    public static final String SEARCH_LINK = "//div[@class='footer']//a[text()='Search']";
+    public static final String SHIPPING_AND_RETURNS_LINK = "//div[@class='footer']//a[text()='Shipping & returns']";
+    public static final String SITEMAP_LINK = "//div[@class='footer']//a[text()='Sitemap']";
+    public static final String FOOTER_MY_ACCOUNT_LINK = "//div[@class='footer']//a[text()='My account']";
+    public static final String HEADER_LOGO_HOMEPAGE_LINK = "//div[@class='header-lower']/div[@class='header-logo']/a";
+    public static final String HEADER_WISHLIST_LINK = "//div[@class='header-upper']//a[@class='ico-wishlist']";
+    public static final String LOADING_ICON = "//div[@id='ajaxBusy']";
+
+    // Dynamic Locator
+    public static final String FOOTER_DYNAMIC_LINK = "//div[@class='footer']//a[text()='%s']";
+    public static final String ADMIN_LEFT_MENU_DYNAMIC_LINK = "//span[text()='%s']/parent::a";
+    public static final String DYNAMIC_UPLOAD_FILES_INPUT_BY_PANEL_ID = "//div[@id='%s']//input[@type='file']";
+    public static final String DYNAMIC_EXPAND_ICON_FIND_BY_PANEL_ID = "//div[@id='%s']//i[contains(@class, 'toggle-icon')]";
+
+    public static final String DYNAMIC_RADIO_BUTTON_BY_ID = "//input[@id='%s']";
+    public static final String DYNAMIC_TEXTBOX_BY_ID = "//input[@id='%s']";
+    public static final String DYNAMIC_BUTTON_BY_VALUE = "//input[@value='%s']";
+}
