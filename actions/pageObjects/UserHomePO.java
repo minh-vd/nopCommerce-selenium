a8f7c4cdf1da5ab789b48ffdf0db8ccd907bd91b
minhvd <minhvd1024@gmail.com>
2021-03-16 01:58:40 +1100

    add page object UserHomePO

---
+++ actions/pageObjects/UserHomePO.java
@@ -0,0 +1,11 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class UserHomePO {
+    WebDriver driver;
+
+    public UserHomePO(WebDriver driver) {
+        this.driver = driver;
+    }
+}
